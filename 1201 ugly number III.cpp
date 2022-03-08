9ba2c86d520a7217a5cf5f01ebba52060835a330
Federico Baldi <federico96.baldi@gmail.com>
2022-03-08 11:34:11 +0100

    Add ugly number III solution

---
+++ 1201 ugly number III.cpp
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/missing-number
+// TLE
+class Solution {
+public:
+    int nthUglyNumber(int n, int a, int b, int c) {
+        int uglyNumbers = 0;
+        int currentUgly = min(a,min(b,c))-1;
+        while (uglyNumbers != n){
+            currentUgly++;
+            if (currentUgly % a == 0 || currentUgly % b == 0 || currentUgly % c == 0){
+                uglyNumbers++;
+                cout << currentUgly << " ";
+            }
+        }
+        return currentUgly;
+    }
+};
+
+// TC: O(log(2e9)) SC: O(1)
+typedef long long ll;
+class Solution {
+public:
+    int countUgly(ll number, ll a, ll b, ll c){
+        int count = 0;
+        count += number / a;
+        count += number / b;
+        count += number / c;
+        count -= number / lcm(a, b);
+        count -= number / lcm(b, c);
+        count -= number / lcm(a, c);
+        count += number / lcm(a, lcm(b, c));
+        return count;
+    }
+    int nthUglyNumber(int n, int a, int b, int c) {
+        int left = 1, right = 2e9;
+        int mid;
+        while (left < right){
+            mid = left + (right - left) / 2;
+            if (countUgly(mid, a, b, c) >= n){
+                right = mid;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};

e7fc378e7a1920fb354ca46b1297040f8c23e020
Federico Baldi <federico96.baldi@gmail.com>
2022-03-14 14:09:16 +0100

    Add simplify path solution

---
+++ 71 simplify path.cpp
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/climbing-stairs
+// recursive TLE TS: O(n) SC: O(1)
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string canonicalPath = "";
+        string word = "";
+        stack<string> stringsPath;
+        for(int index = 0; index < path.size(); index++){
+            if(path[index] != '/'){
+                word += path[index];
+            }
+            if (index == path.size()-1 || path[index] == '/'){
+                if (word != "" && word != "."){
+                    if (word != ".."){
+                        stringsPath.push(word);
+                    }
+                    else if (stringsPath.empty() == false){
+                        stringsPath.pop();
+                    }
+                }
+                word = "";
+            }
+        }
+        while (stringsPath.empty() == false){
+            canonicalPath = "/" + stringsPath.top() + canonicalPath;
+            stringsPath.pop();
+        }
+        if (canonicalPath.empty() == true){
+            canonicalPath = "/";
+        }
+        return canonicalPath;
+    }
+};

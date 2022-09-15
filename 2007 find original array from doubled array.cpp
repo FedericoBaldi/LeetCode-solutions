e0df61d3c9c02de14ae94816c4f7770e3da02a43
Federico Baldi <federico96.baldi@gmail.com>
2022-09-15 15:04:05 +0200

    Add find original array from doubled array solution
    fix of previous commit

--- 2007 find original array from doubled array.cpp
+++ 2007 find original array from doubled array.cpp
@@ -1,34 +1,52 @@
-// https://leetcode.com/problems/time-based-key-value-store
-// set get TS:O(1) SC:O(n) get TS:O(logn) SC:O(n)
-// binary search with upperbound variation.
-class TimeMap {
-private:
-    unordered_map<string, vector<pair<int, string>>> timesMap;
-    
+// https://leetcode.com/problems/find-original-array-from-doubled-array
+// TS:O(nlogn) SC:O(n)
+class Solution {
 public:
-    TimeMap() {
-        
-    }
-    
-    void set(string key, string value, int timestamp) {
-        timesMap[key].push_back({timestamp, value});
-    }
-    
-    string get(string key, int timestamp) {
-        int low = 0;
-        int high = timesMap[key].size();
-        while (low < high){
-            int mid = low + (high - low) / 2;
-            if (timesMap[key][mid].first <= timestamp){
-                low = mid + 1;
-            }
-            else{
-                high = mid;
+    vector<int> findOriginalArray(vector<int>& changed) {
+        if (changed.size() % 2 != 0){
+            return {};
+        }
+        vector<int> original;
+        sort(changed.begin(), changed.end());
+        unordered_map <int, int> map;
+        for (int index = 0; index < changed.size(); index++){
+            map[changed[index]]++;
+        }
+        for (int index = 0; index < changed.size(); index++){
+            int indexDouble = map[changed[index]*2];
+            if (map[changed[index]] != 0){
+                if (map[changed[index] * 2] != 0){
+                    map[changed[index]]--;
+                    map[changed[index] * 2]--;
+                    original.push_back(changed[index]);
+                }
+                else {
+                    return {};
+                }
             }
         }
-        if (low > 0 && low <= timesMap[key].size()){
-            return timesMap[key][low-1].second;
+        return original;
+    }
+};
+// TLE TS:O(n^2) SC:O(n)
+class Solution {
+public:
+    vector<int> findOriginalArray(vector<int>& changed) {
+        sort(changed.begin(), changed.end());
+        bool found = true;
+        for (int index = 0; index < changed.size() && found; index++){
+            found = false;
+            for (int indexK = index+1; indexK < changed.size() && changed[indexK] <= changed[index]*2; indexK++){
+                if (changed[index] == changed[indexK] / 2){
+                    found = true;
+                    changed.erase(changed.begin() + indexK);
+                    break;
+                }
+            }
         }
-        return "";
+        if (found == false){
+            return vector<int>();
+        }
+        return changed;
     }
 };

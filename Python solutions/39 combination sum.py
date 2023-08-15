14802042d78cd2f43c1097cd78696b4fb810e38c
Federico Baldi <federico96.baldi@gmail.com>
2023-08-15 20:48:07 +0200

    re: improve combination sum python solution

--- Python solutions/39 combination sum.py
+++ Python solutions/39 combination sum.py
@@ -1,6 +1,23 @@
 # https://leetcode.com/problems/combination-sum
+# TC: o(2^target * target) SC: o(k * target) k = number of sums
+# Select one path and go down, then select another.
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+    res = []
+    comb = []
+    def helper(target_sum: int, index: int):
+        if target_sum <= 0 or index >= len(candidates):
+            if target_sum == 0:
+                res.append(comb[:])
+            return
+        comb.append(candidates[index])
+        helper(target_sum - candidates[index], index)
+        comb.pop()
+        helper(target_sum, index + 1)
+    helper(target, 0)
+    return res
+        
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         comb = []
         candidates.sort()
         def app (curr_index: int, curr_target: int, curr_comb: List[int]):
